fe89b08321fbbb1b40788a1f83692dbc4ea361a8
Rubem Vasconcelos <rfsv@ic.ufal.br>
2019-07-31 12:59:25 -0300

    Metodos não utilizados removidos

--- app/helpers/meetings_helper.rb
+++ app/helpers/meetings_helper.rb
@@ -1,9 +1,2 @@
 module MeetingsHelper
-  OptionsForRooms = Struct.new(:id, :title)
-
-  def options_for_rooms
-    Room.all.map do |key, value|
-      OptionsForRooms.new(key, value)
-    end
-  end
 end
